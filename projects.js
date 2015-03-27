451d618d6f41d1715a1b741325971da31dc7993e
U-SPARKFUN\chris.taylor <chris.taylor@SFHQ_EG_CTAYLOR.sparkfun.local>
2015-03-27 15:07:57 -0600

    added animations to projects.html.

--- projects.js
+++ projects.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+		$("#further-projects").hide();
+		$(".separator").mouseenter(function() {
+			$(this).animate({
+				opacity: 0.25,
+				top: '-=2px'
+			}, 'fast');
+		});
+		$(".separator").mouseleave(function() {
+			$(this).animate({
+				opacity: 1.0,
+				top: '+=2px'
+			}, 'fast');
+		});
+		$(".separator").click(function() {
+			if($('#further-projects').is(':visible')) {
+				$("#further-projects").hide(200);
+			}
+			else {
+				$("#further-projects").show(500);
+			};
+		});
 		
-		$(".separator").animate({height: "+=20px"});
 });
